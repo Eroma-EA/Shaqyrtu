aa27f42385ef3552855443a2aaaf02b4f0894b79
Shaqyrtu App <wedding@shaqyrtu.kz>
2026-06-30 15:44:53 +0500

    Add dynamic Open Graph metadata and WhatsApp preview image

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,20 @@
 import "./globals.css";
 
+import { weddingConfig } from "../data/config";
+
+const titleText = `${weddingConfig.groom} & ${weddingConfig.bride} ${weddingConfig.date}`;
+const descText = `Құрметті қонақтар! ✨\n\n${weddingConfig.groom} пен ${weddingConfig.bride}ның үйлену тойына арналған шақыруды мына сілтеме арқылы көре аласыздар 👇`;
+
 export const metadata = {
-  title: "Шақыру билеті",
-  description: "Үйлену тойына шақыру",
+  title: titleText,
+  description: descText,
+  openGraph: {
+    title: titleText,
+    description: descText,
+    type: 'website',
+    locale: 'kk_KZ',
+    siteName: 'Шақыру билеті',
+  },
 };
 
 export default function RootLayout({ children }) {
